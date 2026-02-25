4598d968419f0772ca000190bf7ff25d582aa502
Anh Vy Tran <tr.anhvy.work@gmail.com>
2026-02-25 14:06:17 +0700

    Refactor/Auth Error Response (#4)
    * feat: add error enums
    
    * feat: adjust error response

--- src/auth/jwt/jwtAuth.guard.ts
+++ src/auth/jwt/jwtAuth.guard.ts
@@ -1,15 +1,45 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { AuthErrorCode } from "../enums/authErrorCode.enum";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
-    handleRequest(err: any, user: any, info: any) {
+  handleRequest(err: any, user: any, info: any) {
     if (err || !user) {
+      // Missing token
+      if (!info) {
+        throw new UnauthorizedException({
+          success: false,
+          code: AuthErrorCode.TOKEN_MISSING,
+          message: "Missing access token",
+        });
+      }
+
+      // Token expired
+      if (info?.name === "TokenExpiredError") {
+        throw new UnauthorizedException({
+          success: false,
+          code: AuthErrorCode.TOKEN_EXPIRED,
+          message: "Access token expired",
+        });
+      }
+
+      // Invalid token
+      if (info?.name === "JsonWebTokenError") {
+        throw new UnauthorizedException({
+          success: false,
+          code: AuthErrorCode.TOKEN_INVALID,
+          message: "Invalid access token",
+        });
+      }
+
       throw new UnauthorizedException({
-        status: 'error',
-        message: 'Unauthorized',
+        success: false,
+        code: AuthErrorCode.UNAUTHORIZED,
+        message: "Unauthorized",
       });
     }
+
     return user;
   }
 }
